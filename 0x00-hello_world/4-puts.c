69c5f2b9b967ac65b9d0480feeb9dcc0d2a20e9f
Mzpenelope <phillipaaidoo12345@gmail.com>
2023-04-20 01:29:19 -0700

    C-Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string to the console
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle\n");
+    return (0);
+}
