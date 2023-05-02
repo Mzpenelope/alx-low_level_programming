f04eec759da937dbd56b733ab532053768855d76
Mzpenelope <phillipaaidoo12345@gmail.com>
2023-05-02 10:27:14 -0700

    UPDATE

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,16 @@
 void puts_half(char *str)
 {
 int index, half;
-
 index = 0;
 while (str[index] != 0)
 index++;
-
 half = index / 2;
-
 if (index % 2 == 1)
 half++;
-
-while (half < index);
+while (half < index)
 {
 _putchar(str[half]);
 half++;
 }
-_putchar(10);
+_putchar(n);
 }
