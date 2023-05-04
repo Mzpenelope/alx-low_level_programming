fc9008ec11806dfca254e00aea1d6959720014be
Mzpenelope <phillipaaidoo12345@gmail.com>
2023-05-04 08:37:48 -0700

    UPDATE

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,15 +6,17 @@
  * @n: number of elements to concatenate in
  * Return: pointer to resulting `dest`
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
 int i, c;
 
-for (i = 0; dest[i] != 0; i++);
+for (i = 0; dest[i] != 0; i++)
 ;
-for (c = 0; src[j] != 0 && n > 0; c++, n--, f++);
+for (c = 0; src[c] != 0 && n > 0; c++, n--)
 {
 dest[i] = src[c];
+i++;
 }
+dest[i] = 0; /* add null terminator to the end of `dest` */
 return (dest);
 }
