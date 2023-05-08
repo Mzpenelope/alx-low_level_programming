f6d63c0f6dc519220fe73b7e3ffba23d99df0d8c
Mzphill <phillipaaidoo12345@gmail.com>
2023-05-08 22:42:41 +0100

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+0. char *_memset(char *s, char b, unsigned int n);
+
+1. char *_memcpy(char *dest, char *src, unsigned int n);
+
+2. char *_strchr(char *s, char c);
+
+3. unsigned int _strspn(char *s, char *accept);
+
+4. char *_strpbrk(char *s, char *accept);
+
+5. char *_strstr(char *haystack, char *needle);
+
+6. void print_chessboard(char (*a) [8]);
+
+7. void print_diagsums(int *a, int size);
+
+8. int _putchar(char c);
+
+9. void set_string(char **s, char *to);
