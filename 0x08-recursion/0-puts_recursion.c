abcc551beefebcfebb89cf12188da32fcc0b94f1
Mzphill <phillipaaidoo12345@gmail.com>
2023-05-10 17:09:12 +0100

    0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,16 @@
 /**
  * _puts_recursion - print a string
  * @s:string
+ *
  * Return:void
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-_putchar(*s);
-_puts_recursion(s + 1);
-}
-else
-{
-_putchar(n);
-}
-
+	if (*s == '\0') /*base case*/
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
