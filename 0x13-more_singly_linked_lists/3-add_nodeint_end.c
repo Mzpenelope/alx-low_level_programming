619c5c2f33546b3ff8e738eb5810a1553e4434c8
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-07 09:21:52 +0100

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,6 @@
 #include "lists.h"
-
 /**
- * add_nodeint_end - adds a node to the end of a linked list
+ * add_nodeint_end - adds node to end of linked list
  * @head: pointer to the head of the list
  * @n: number to be used as content
  *
